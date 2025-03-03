2ad700c236b9168c4b028053e559ee8f08fdf73e
Hassan Algindy <158037441+HASSAN123THE@users.noreply.github.com>
2025-03-03 03:46:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
-function main() {
-    alert("الصفحه غير جاهزه للاستعمال علي الهواتف فقط علي الكومبيوتر او الابتب 😊"); 
-    
-    let navbar = document.createElement("div");
-    
-}
 
-main();
+window.onload = function() {
+    alert("الصفحه غير جاهزه للاستعمال علي الهواتف فقط علي الكومبيوتر او الابتب 😊");
+    main(); 
+};
